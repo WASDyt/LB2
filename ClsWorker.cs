280e3eddf92e6e8bf40cd069254c25d800f307fa
WASDyt <wasd50983@gmail.com>
2023-04-04 10:17:05 +0300

    Добавьте файлы проекта.

---
+++ ClsWorker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOOPClasses //пространство имен проекта
+{
+    internal class ClsWorker
+    {
+        //поля
+        private string fio;
+        private DateTime years;
+        private string post;
+        // свойства для доступа к полям
+        public string FIO
+        {
+            get { return fio; }//возвращает значение поля
+            set { fio = value; } //устанавливает значение поля
+        }
+
+        public string Post
+        {
+            get { return post; } //возвращает значение поля
+            set { post = value; } //устанавливает значение поля
+        }
+        public DateTime Years
+        {
+            get { return years; }
+            set { years = value; }
+        }
+
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClsWorker()
+        {
+            fio = "Лебедев М.Д.";
+            post = "Ликино-Дулево";
+            years = new DateTime(2005, 02, 01);
+        }
+        //конструктор с параметрами
+        public ClsWorker(string f, DateTime y, string p) 
+        {
+            fio = f;
+            years = y;
+            post = p;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+            string line = $"Рабочий {fio}, у него должность {post} поступил на руботу {years}" + "\n";
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Личные данные рабочего");
+            streamWriter.WriteLine($"Должность: {post}");
+            streamWriter.WriteLine($"ФИО: {fio}");
+            streamWriter.WriteLine($"Дата поступления: {years.ToString()}");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleAppOOPClasses;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LB2
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClsWorker worker = new ClsWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void BtnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            worker.FIO = TxtFIO.Text;
+            worker.Post = TxtPost.Text;
+            worker.Years = (DateTime)DtYears.SelectedDate;
+
+            LstWorkers.Items.Clear();
+            LstWorkers.Items.Add(worker.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            worker.SaveToFile(saveFileDialog.FileName);
+        }
+
+    }
+}
